c1e21289a659ad7757a212c7ca20c7567e7a76bd
Chris McGrath <chris@octopod.info>
2011-04-14 18:02:21 +0100

    Small tweak to see if I can get barista/sprockets playing nice

--- lib/sprockets_rails3/railtie.rb
+++ lib/sprockets_rails3/railtie.rb
@@ -5,7 +5,7 @@ require 'rack/sprockets'
 module SprocketsRails3
  class Railtie < Rails::Railtie
    initializer 'sprockets_rails3' do |app|
-     app.middleware.use Rack::Sprockets
+     app.middleware.use Rack::Sprockets, :source => 'tmp/javascripts', :load_path => ['tmp/javascripts', 'vendor/javascripts']
    end
  end
 end
